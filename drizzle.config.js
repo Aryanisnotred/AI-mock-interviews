91328e948a36f3768b3c591a80af7db170e64616
Aryanisnotred <enterjibil98@gmail.com>
2025-07-18 21:29:56 +0530

    Remove all project files except .git and ai-mock-interview directory

--- drizzle.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-export default defineConfig({
-  dialect: "postgresql",
-  schema: "./utils/schema.js",
-  dbCredentials: {
-    url: "postgresql://neondb_owner:ebgkdSMYo13f@ep-winter-rice-a5zy7h81.us-east-2.aws.neon.tech/ai-mock-interview?sslmode=require",
-  }
-});
